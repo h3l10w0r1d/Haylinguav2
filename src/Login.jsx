1fc933c3aa7b88e3bb1baed80466aa867422adf1
Grigor <95748355+h3l10w0r1d@users.noreply.github.com>
2025-11-23 20:01:31 +0400

    Create Login component for user authentication

---
+++ src/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Login</h1>
+      <p>This will be your login page.</p>
+    </div>
+  );
+}
