b83e4fb3ebec59fa2d1f7054d5414ae5a9ddb0f6
Grigor <95748355+h3l10w0r1d@users.noreply.github.com>
2025-11-23 20:01:55 +0400

    Create Signup.jsx

---
+++ src/Signup.jsx
@@ -0,0 +1,8 @@
+export default function Signup() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Signup</h1>
+      <p>This will be your signup page.</p>
+    </div>
+  );
+}
