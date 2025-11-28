bdf09b8f060140f01c68ff9376faa570d746ccf0
Grigor <95748355+h3l10w0r1d@users.noreply.github.com>
2025-11-28 11:42:37 +0400

    Change DATABASE_URL to use os.getenv

--- backend/database.py
+++ backend/database.py
@@ -3,9 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
