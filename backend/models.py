3c1839baf71632b2bc4f0747d0fff650ed43b307
Armen Ghazaryan <95748355+h3l10w0r1d@users.noreply.github.com>
2025-11-29 17:54:03 +0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String
 from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Text
+from sqlalchemy.orm import relationship
+
 
 class User(Base):
     __tablename__ = "users"
