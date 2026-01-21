b3823ed80da8a533fab79696597d4dab98a1eea5
Armen Ghazaryan <95748355+h3l10w0r1d@users.noreply.github.com>
2026-01-21 13:50:14 +0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-
+    # Prod Domain
+    "https://haylingua.am/",
     # Vercel production
     "https://haylinguav2.vercel.app",
 
