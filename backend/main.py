bd559385f77219ace778af13ddc963a871ac53e4
Armen Ghazaryan <95748355+h3l10w0r1d@users.noreply.github.com>
2026-01-21 13:50:28 +0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     # Prod Domain
-    "https://haylingua.am/",
+    "https://haylingua.am",
     # Vercel production
     "https://haylinguav2.vercel.app",
 
